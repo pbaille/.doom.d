4c4eea1cf1930465a9c1f4f7e0b59127682af96a
pbaille <pierrebaille@gmail.com>
2023-04-18 19:04:50 +0200

    format user package root file pb.el

--- pb/pb.el
+++ pb/pb.el
@@ -1,4 +1,31 @@
-;;; pb/pb.el -*- lexical-binding: t; -*-
+;;; pb.el --- Multipurpose personal package -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Pierre Baille
+
+;; Author: Pierre Baille <pierrebaille@gmail.com>
+;; Keywords: lisp
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/pbaille/pb.el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; personal utility belt
+
+;;; Code:
 
 (load "~/.doom.d/pb/misc.el")
 (load "~/.doom.d/pb/s-expr.el")
@@ -6,6 +33,5 @@
 (load "~/.doom.d/pb/fennel.el")
 (load "~/.doom.d/pb/reaper.el")
 
-(print "pb loaded")
-
 (provide 'pb)
+;;; pb.el ends here
