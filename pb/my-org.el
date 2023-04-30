f01c2b2594f1d9c163c8add12d5fa62641dfa224
pbaille <pierrebaille@gmail.com>
2023-04-30 20:37:56 +0200

    add my-org-put
    let you insert some content in an org file at the given path
    if the targetted header do not exists it will be created

--- pb/my-org.el
+++ pb/my-org.el
@@ -5,31 +5,57 @@
 
 ;; olp stands for outline path
 ;; this switches to the scratch org buffer and go to one of its sub header
-(progn (switch-to-buffer "scratch.org")
-       (goto-char (org-find-olp (list "~/org/scratch.org" "top" "three" "3.2"))))
 
-(defun my-org-find-or-create-olp-aux (file current-marker current-path remaining-path)
-  (print (list "-" current-marker current-path remaining-path))
-  (or (let ((next-marker (org-find-olp (cons file current-path))))
-        (print next-marker)
-        (if next-marker
-            (if remaining-path
-                (my-org-find-or-create-olp-aux file
-                                               current-marker
-                                               (append current-path (list (car remaining-path)))
-                                               (cdr remaining-path))
-              (list
-               :file file
-               :at current-path
-               :to-create remaining-path
-               :mark next-marker))))
-      (list
-       :file file
-       :at current-path
-       :to-create remaining-path
-       :mark current-marker)))
+(require 'km)
+(require 'org)
+
+'(progn (switch-to-buffer "scratch.org")
+        (goto-char (org-find-olp (list "~/org/scratch.org" "top" "three" "3.2"))))
+
+(defnk my-org-find-or-create-olp-aux
+  (:as opts
+       file current-path remaining-path)
+  (let* ((next-path (append current-path (list (car remaining-path))))
+         (next-marker (ignore-errors (org-find-olp (cons file next-path)))))
+    (if next-marker
+        (let* ((remaining-next (cdr remaining-path))
+               (next-opts (km-put opts
+                                  :marker next-marker
+                                  :current-path next-path
+                                  :remaining-path remaining-next)))
+          (if remaining-next
+              (my-org-find-or-create-olp-aux next-opts)
+            next-opts))
+      opts)))
 
 (defun my-org-find-or-create-olp (file path)
-  (my-org-find-or-create-olp-aux file 0 (list (car path)) (cdr path)))
+  (km-letk ((remaining-path marker)
+            (my-org-find-or-create-olp-aux
+             (km :file file
+                 :marker 0
+                 :current-path ()
+                 :remaining-path path)))
 
-(my-org-find-or-create-olp "~/org/scratch.org" (list "top" "tao"))
+           (switch-to-buffer (get-file-buffer file))
+           (goto-char marker)
+           (cl-loop for i in remaining-path
+                    do (org-insert-subheading 0) (insert i))))
+
+(defun my-org-put (file path content)
+  (my-org-find-or-create-olp file path)
+  (cond ((stringp content) (evil-open-below 1) (insert content))
+        ((km? content)
+         (org-set-tags (km-get content :tags))
+         (evil-open-below 1) (insert (km-get content :text)))))
+
+
+'(progn
+  (my-org-find-or-create-olp "~/org/scratch.org" (list "top" "tao"))
+  (my-org-find-or-create-olp "~/org/scratch.org" (list "top" "tao" "baz" "iop"))
+  (my-org-put "~/org/scratch.org" (list "top" "tao" "baz" "iop")
+              "hello you")
+  (my-org-put "~/org/scratch.org" (list "top" "tao" "baz" "iop")
+              (km :tags (list "pouet" "foo")
+                  :text "very interesting indeed")))
+
+(provide 'my-org)
