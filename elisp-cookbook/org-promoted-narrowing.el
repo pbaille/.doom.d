0db8915e3bb61c23dd88760250cf1f1768d05218
pbaille <pierrebaille@gmail.com>
2023-05-04 05:26:53 +0200

    improve robustness of org-promoted-narrowing.el
    handles calling widen on non narrowed node
    handles calling narrow on a narrow node
    don't use local state but org properties

--- elisp-cookbook/org-promoted-narrowing.el
+++ elisp-cookbook/org-promoted-narrowing.el
@@ -1,13 +1,13 @@
 ;;; elisp-cookbook/org-promoted-narrowing.el -*- lexical-binding: t; -*-
 
-(setq my-org-original-level 1)
-
 (defun my-org-narrow-to-subtree-and-promote ()
   "Narrow to the current subtree and promote it to a top-level heading.
    Store the original level of the heading in a buffer-local variable."
   (interactive)
+  (if (buffer-narrowed-p)
+      (my-org-widen-and-demote))
   (org-narrow-to-subtree)
-  (setq-local my-org-original-level (org-current-level))
+  (org-set-property "ORIGINAL_HEADING_LVL" (number-to-string (org-current-level)))
   (while (> (org-current-level) 1)
     (org-promote-subtree))
   (outline-show-all))
@@ -15,6 +15,11 @@
 (defun my-org-widen-and-demote ()
   "Widen the buffer and demote the heading to its original level."
   (interactive)
-  (while (> my-org-original-level (org-current-level))
-    (org-demote-subtree))
+  (if (buffer-narrowed-p)
+      (save-excursion
+        (goto-char (point-min))
+        (let ((original-level (string-to-number (org-entry-get (point-min) "ORIGINAL_HEADING_LVL"))))
+          (while (> original-level (org-current-level))
+            (org-demote-subtree)))
+        (org-delete-property "ORIGINAL_HEADING_LVL")))
   (widen))
