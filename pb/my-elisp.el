5c7416b359f45a4ddd3d5e471be1220ed888921a
pbaille <pierrebaille@gmail.com>
2024-06-14 23:08:52 +0200

    elisp send to ielm action

---
+++ pb/my-elisp.el
@@ -0,0 +1,14 @@
+;;; pb/my-elisp.el -*- lexical-binding: t; -*-
+
+(defun my-elisp_send-expression-to-ielm ()
+  "Evaluate the expression at point and print the result in the IELM buffer."
+  (interactive)
+  (if (get-buffer "*ielm*")
+      (let* ((expr (or (thing-at-point 'symbol t)
+                       (thing-at-point 'sexp t))))
+        (with-current-buffer "*ielm*"
+          (goto-char (point-max))
+          (insert expr)
+          (ielm-send-input)))))
+
+(provide 'my-elisp)
