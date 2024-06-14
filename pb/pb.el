9e01d4e0d4cb849faadd46d35312cb5801d11923
pbaille <pierrebaille@gmail.com>
2024-06-14 23:09:04 +0200

    require my-elisp and my-walk in pb.el

--- pb/pb.el
+++ pb/pb.el
@@ -29,11 +29,13 @@
 
 (load "~/.doom.d/pb/misc.el")
 (load "~/.doom.d/pb/s-expr.el")
+(load "~/.doom.d/pb/my-walk.el")
 (load "~/.doom.d/pb/sidebars.el")
 (load "~/.doom.d/pb/my-symex.el")
 (load "~/.doom.d/pb/my-cider.el")
 (load "~/.doom.d/pb/my-fennel.el")
 (load "~/.doom.d/pb/my-dired.el")
+(load "~/.doom.d/pb/my-elisp.el")
 (load "~/.doom.d/pb/reaper.el")
 (load "~/.doom.d/pb/my-fold.el")
 (load "~/.doom.d/pb/my-ibuffer.el")
