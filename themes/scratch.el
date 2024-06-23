8cd287ef3b68cb79801d3dc639b938bec38484f7
pbaille <pierrebaille@gmail.com>
2024-06-23 20:44:08 +0200

    remove invalid top level forms

--- themes/scratch.el
+++ themes/scratch.el
@@ -8,15 +8,10 @@
           (lighten .5)
           (hue-wheel 6))
 
-("#e4e498" "#98e498" "#98e4e4" "#9898e4" "#e498e4" "#e49898")
-
-
 (let ((base (pb-color (pb-color_random)
                       (desaturate .4))))
   (list (pb-color_darken base 0.6)
         (pb-color_lighten (pb-color_complementary base) 0.6)))
-
-("#1c3c37" "#e2c3c7")
 
 (defun rand-color ()
   (pb-color_random))
@@ -27,18 +22,6 @@
                   (complementary)
                   (set-lightness (- 1 (pb-color_lightness c))))))
 
-("#a45243" "#5aadbc")
-
-("#1cf8f8" "#e30707")
-
-("#52d9e9" "#ac2616")
-
-("#60f9a7" "#9e0657")
-
-("#78de94" "#87206a")
-
-("#cfdc73" "#30228c")
-
 (defun color-walk (data f)
   ""
   (cond ((consp data)
@@ -48,21 +31,12 @@
          (funcall f data))
         (t data)))
 
-(defun pb-palette (base)
-  "Generate a color palette based on BASE (hex color)."
-  (let ((colors (pb-color_overview base)))
-    (list :light
-          (list :fg
-                :bg )
-
-          :dark
-          (list ))))
-
 (defun pprob (x)
   (pp x)
   nil)
 
 (defmacro fn (args &rest body)
+  "Like lambda but with a shorter name and destructuring."
   (let ((bindings (cl-loop for i from 0 to (- (length args) 1)
                            collect (cons (gensym (format "arg_%s" i)) (nth i args)))))
     `(lambda ,(mapcar #'car bindings)
@@ -72,189 +46,130 @@
                     (reverse bindings)
                     (cons 'progn body)))))
 
-(pprob (let* ((bg (pb-color_from-hsl
-                   (list (pb-color_random-value) .5 .2)))
 
-              (fg (pb-color_complementary bg))
+(defun pb-theme_palette (fg)
+  "Derive a theme palette from a foreground color FG."
+  (let* ((bg (pb-color_complementary fg))
 
-              (decline (lambda (x)
-                         (let* ((dimed (pb-color_neutralize-lightness x .1))
-                                (alt (pb-color_update-hue dimed (lambda (x) (+ .1 x)))))
-                           (list :main x
-                                 :dim dimed
-                                 :alt alt))))
+         (decline (lambda (x)
+                    (let* ((dimed (pb-color_neutralize-lightness x .1))
+                           (vivid (pb-color_exacerbate-lightness x .3))
+                           (alt (pb-color_update-hue dimed (lambda (x) (+ .1 x))))
+                           (alt2 (pb-color_update-hue dimed (lambda (x) (- x .1)))))
+                      (list :main x
+                            :dim dimed
+                            :vivid vivid
+                            :alt alt
+                            :alt2 alt2
+                            :accent (pb-color_complementary-hue x)))))
 
-              (colors (km_into ()
-                               (seq-mapn #'cons
-                                         (list :yellow :green :cyan :blue :magenta :red )
-                                         (pb-color fg
-                                                   (set-hue 0)
-                                                   (set-saturation .7)
-                                                   (hue-wheel 6)))))
+         (colors (km_into ()
+                          (seq-mapn #'cons
+                                    (list :yellow :green :cyan :blue :magenta :red )
+                                    (pb-color fg
+                                              (set-hue 0)
+                                              (set-saturation .8)
+                                              (hue-wheel 6)))))
 
-              (decline-color
-               (lambda (c)
-                 (let ((fg (list :main c
-                                 :warmer (pb-color_warm c .1)
-                                 :cooler (pb-color_cool c .1)
-                                 :faint (pb-color_neutralize-lightness (pb-color_desaturate c .7)
-                                                                       .5))))
-                   (list :fg fg
-                         :bg (color-walk fg #'pb-color_complementary-lightness)))))
+         (decline-color
+          (lambda (c)
+            (let ((fg (list :main c
+                            :warmer (pb-color_warm c .1)
+                            :cooler (pb-color_cool c .1)
+                            :faint (pb-color c
+                                             (desaturate .7)
+                                             (neutralize-lightness .5)))))
+              (list :fg fg
+                    :bg (color-walk fg #'pb-color_complementary-lightness))))))
 
-              (dark (list :bg (funcall decline bg)
-                          :fg (funcall decline fg)
-                          :colors (km_map
-                                   colors
-                                   (fn ((name . c))
-                                       (cons name (funcall decline-color c))))))
+    (list :bg (funcall decline bg)
+          :fg (funcall decline fg)
+          :colors (km_map
+                   colors
+                   (fn ((name . c))
+                       (cons name (funcall decline-color c)))))))
 
-              (light (color-walk dark #'pb-color_complementary-lightness)))
+(defun km_flat (m sep)
+  (seq-mapcat (fn ((k . v))
+                  (if (km? v)
+                      (km_map (km_flat v sep)
+                              (fn ((k1 . v1))
+                                  (cons (intern
+                                         (concat ":"
+                                                 (pb_keyword-name k)
+                                                 sep
+                                                 (pb_keyword-name k1)))
+                                        v1)))
+                    (list k v)))
+              (km_entries m)))
 
-         (list :dark dark
+
+(progn :macros
+
+       (defmacro -> (x &rest forms)
+         (seq-reduce
+          (lambda (result form)
+            (if (seqp form)
+                `(,(car form) ,result ,@(cdr form))
+              (list form result)))
+          forms
+          x))
+
+       (defmacro dlet (bindings &rest body)
+         (seq-reduce
+          (lambda (ret binding)
+            (cl-destructuring-bind (pat expr) binding
+              (if (symbolp pat)
+                  `(let (,binding) ,ret)
+                `(cl-destructuring-bind ,@binding ,ret))))
+          (reverse (sq_partition 2 2 bindings))
+          (cons 'progn body)))
+
+       (defmacro >_ (&rest forms)
+         "Thread the first argument into following FORMS.
+using the _ placeholder to determine threaded value positioning."
+         (cl-destructuring-bind (ret . bindings) (reverse forms)
+           `(let* ,(seq-reduce
+                    (lambda (bindings form)
+                      (cons (list '_ form) bindings))
+                    bindings
+                    (list))
+              ,ret)))
+
+       (defmacro f_ (&rest body)
+         `(lambda (_)
+            ,@body)))
+
+(defun pb-theme_transform-color-key (k separator)
+  (pcase (string-split (symbol-name k) separator)
+    (`(":colors" . ,xs)
+     (intern (concat ":"
+                     (pcase xs
+                       (`(,color "fg" "main") color)
+                       (`(,color "fg" ,variant) (concat color "-" variant))
+                       (`(,color "bg" ,variant) (concat "bg-" color "-"
+                                                        (pcase variant
+                                                          ("main" "intense")
+                                                          ("faint" "subtle")
+                                                          (_ variant))))))))))
+
+(defun pb-theme_rand-palette ()
+  (-> (pb-color_from-hsl
+       (list (pb-color_random-value) .2 .8))
+      (pb-theme_palette)
+      (km_flat "-")
+      (km_map-keys
+       (f_ (pb_keyword-to-symbol
+            (or (pb-theme_transform-color-key _ "-") _))))
+      (km_map-vals #'list)
+      (km_entries))
+
+  )
+
+(pprob (let* ((palette (pb-theme_palette (pb-color_from-hsl
+                                          (list (pb-color_random-value) .5 .8))))
+
+              (light (color-walk palette #'pb-color_complementary-lightness)))
+
+         (list :dark palette
                :light light)))
-(:dark
- (:bg
-  (:main "#4c4319" :dim "#574d1c" :alt "#3d571b")
-  :fg
-  (:main "#b3bce6" :dim "#a7b1e2" :alt "#c0a6e2")
-  :colors
-  (:yellow
-   (:fg
-    (:main "#efefa9" :warmer "#f0e698" :cooler "#d7d7b1" :faint "#b7b793")
-    :bg
-    (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48"))
-   :green
-   (:fg
-    (:main "#a9efa9" :warmer "#b1e698" :cooler "#98d7b1" :faint "#93b793")
-    :bg
-    (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48"))
-   :cyan
-   (:fg
-    (:main "#a9efef" :warmer "#b1e6d7" :cooler "#98d7f0" :faint "#93b7b7")
-    :bg
-    (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c"))
-   :blue
-   (:fg
-    (:main "#a9a9ef" :warmer "#b1a7d7" :cooler "#9898f0" :faint "#9393b7")
-    :bg
-    (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c"))
-   :magenta
-   (:fg
-    (:main "#efa9ef" :warmer "#f0a7d7" :cooler "#d798f0" :faint "#b793b7")
-    :bg
-    (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c"))
-   :red
-   (:fg
-    (:main "#efa9a9" :warmer "#f0a798" :cooler "#d798b1" :faint "#b79393")
-    :bg
-    (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848"))))
- :light
- (:bg
-  (:main "#e6ddb3" :dim "#e3d8a8" :alt "#cae3a8")
-  :fg
-  (:main "#18214b" :dim "#1d2658" :alt "#371d59")
-  :colors
-  (:yellow
-   (:fg
-    (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48")
-    :bg
-    (:main "#efefaa" :warmer "#efe598" :cooler "#d6d7b0" :faint "#b7b793"))
-   :green
-   (:fg
-    (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48")
-    :bg
-    (:main "#aaefaa" :warmer "#b1e798" :cooler "#98d6b1" :faint "#93b793"))
-   :cyan
-   (:fg
-    (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c")
-    :bg
-    (:main "#aaefef" :warmer "#b2e7d8" :cooler "#98d5ef" :faint "#93b7b7"))
-   :blue
-   (:fg
-    (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c")
-    :bg
-    (:main "#aaaaef" :warmer "#b0a7d7" :cooler "#9898ef" :faint "#9393b7"))
-   :magenta
-   (:fg
-    (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c")
-    :bg
-    (:main "#efaaef" :warmer "#f0a8d6" :cooler "#d798ef" :faint "#b793b7"))
-   :red
-   (:fg
-    (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848")
-    :bg
-    (:main "#efaaaa" :warmer "#efa798" :cooler "#d698b1" :faint "#b79393")))))
-nil
-
-(:dark
- (:bg
-  (:main "#193b4c" :dim "#1c4357" :alt "#1b1f57" :colors
-         (:yellow
-          (:fg
-           (:main "#efefa9" :warmer "#f0e698" :cooler "#d7d7b1" :faint "#b7b793")
-           :bg
-           (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48"))
-          :green
-          (:fg
-           (:main "#a9efa9" :warmer "#b1e698" :cooler "#98d7b1" :faint "#93b793")
-           :bg
-           (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48"))
-          :cyan
-          (:fg
-           (:main "#a9efef" :warmer "#b1e6d7" :cooler "#98d7f0" :faint "#93b7b7")
-           :bg
-           (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c"))
-          :blue
-          (:fg
-           (:main "#a9a9ef" :warmer "#b1a7d7" :cooler "#9898f0" :faint "#9393b7")
-           :bg
-           (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c"))
-          :magenta
-          (:fg
-           (:main "#efa9ef" :warmer "#f0a7d7" :cooler "#d798f0" :faint "#b793b7")
-           :bg
-           (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c"))
-          :red
-          (:fg
-           (:main "#efa9a9" :warmer "#f0a798" :cooler "#d798b1" :faint "#b79393")
-           :bg
-           (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848"))))
-  :fg
-  (:main "#e6c3b3" :dim "#e2b9a7" :alt "#e2dca6"))
- :light
- (:bg
-  (:main "#b3d5e6" :dim "#a8cee3" :alt "#a8ace3" :colors
-         (:yellow
-          (:fg
-           (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48")
-           :bg
-           (:main "#efefaa" :warmer "#efe598" :cooler "#d6d7b0" :faint "#b7b793"))
-          :green
-          (:fg
-           (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48")
-           :bg
-           (:main "#aaefaa" :warmer "#b1e798" :cooler "#98d6b1" :faint "#93b793"))
-          :cyan
-          (:fg
-           (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c")
-           :bg
-           (:main "#aaefef" :warmer "#b2e7d8" :cooler "#98d5ef" :faint "#93b7b7"))
-          :blue
-          (:fg
-           (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c")
-           :bg
-           (:main "#aaaaef" :warmer "#b0a7d7" :cooler "#9898ef" :faint "#9393b7"))
-          :magenta
-          (:fg
-           (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c")
-           :bg
-           (:main "#efaaef" :warmer "#f0a8d6" :cooler "#d798ef" :faint "#b793b7"))
-          :red
-          (:fg
-           (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848")
-           :bg
-           (:main "#efaaaa" :warmer "#efa798" :cooler "#d698b1" :faint "#b79393"))))
-  :fg
-  (:main "#4b2818" :dim "#582f1d" :alt "#59531d")))
-nil
