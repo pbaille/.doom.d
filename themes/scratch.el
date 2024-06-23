1bd609de887f2dc7031b9bf7d562c6bc1d4ad8a6
pbaille <pierrebaille@gmail.com>
2024-06-23 10:20:15 +0200

    theme scratch

--- themes/scratch.el
+++ themes/scratch.el
@@ -1,5 +1,6 @@
 ;;; themes/scratch.el -*- lexical-binding: t; -*-
 
+(require 'km)
 (require 'pb-color)
 
 (pb-color "red"
@@ -61,45 +62,199 @@
   (pp x)
   nil)
 
-(pprob (let* ((bg (pb-color_from-hsl (list (pb-color_random-value)
-                                           .5
-                                           .2)))
-              (complementary-lightness (lambda (x)
-                                         (pb-color_set-lightness x
-                                                                 (- 1 (pb-color_lightness x)))))
+(defmacro fn (args &rest body)
+  (let ((bindings (cl-loop for i from 0 to (- (length args) 1)
+                           collect (cons (gensym (format "arg_%s" i)) (nth i args)))))
+    `(lambda ,(mapcar #'car bindings)
+       ,(seq-reduce (lambda (ret x)
+                      `(cl-destructuring-bind ,(cdr x) ,(car x)
+                         ,ret))
+                    (reverse bindings)
+                    (cons 'progn body)))))
 
-              (dim (lambda (x by)
-                     (if (> (pb-color_lightness x) 0.5)
-                         (pb-color_darken x by)
-                       (pb-color_lighten x by))))
-
-              (alt (lambda (x by)
-                     (pb-color_set-hue x
-                                       (mod (+ by (pb-color_hue x)) 1))))
-
-              (decline (lambda (x)
-                         (let ((dimed (funcall dim x .1)))
-                           (list :main x
-                                 :dim dimed
-                                 :alt (funcall alt dimed .2)))))
+(pprob (let* ((bg (pb-color_from-hsl
+                   (list (pb-color_random-value) .5 .2)))
 
               (fg (pb-color_complementary bg))
 
+              (decline (lambda (x)
+                         (let* ((dimed (pb-color_neutralize-lightness x .1))
+                                (alt (pb-color_update-hue dimed (lambda (x) (+ .1 x)))))
+                           (list :main x
+                                 :dim dimed
+                                 :alt alt))))
+
+              (colors (km_into ()
+                               (seq-mapn #'cons
+                                         (list :yellow :green :cyan :blue :magenta :red )
+                                         (pb-color fg
+                                                   (set-hue 0)
+                                                   (set-saturation .7)
+                                                   (hue-wheel 6)))))
+
+              (decline-color
+               (lambda (c)
+                 (let ((fg (list :main c
+                                 :warmer (pb-color_warm c .1)
+                                 :cooler (pb-color_cool c .1)
+                                 :faint (pb-color_neutralize-lightness (pb-color_desaturate c .7)
+                                                                       .5))))
+                   (list :fg fg
+                         :bg (color-walk fg #'pb-color_complementary-lightness)))))
+
               (dark (list :bg (funcall decline bg)
-                          :fg (funcall decline fg)))
-              (light (color-walk dark complementary-lightness)))
+                          :fg (funcall decline fg)
+                          :colors (km_map
+                                   colors
+                                   (fn ((name . c))
+                                       (cons name (funcall decline-color c))))))
+
+              (light (color-walk dark #'pb-color_complementary-lightness)))
+
          (list :dark dark
                :light light)))
 (:dark
  (:bg
-  (:main "#44194c" :dim "#56305d" :alt "#5d3230")
+  (:main "#4c4319" :dim "#574d1c" :alt "#3d571b")
   :fg
-  (:main "#bbe6b3" :dim "#a8cfa1" :alt "#a0cccf"))
+  (:main "#b3bce6" :dim "#a7b1e2" :alt "#c0a6e2")
+  :colors
+  (:yellow
+   (:fg
+    (:main "#efefa9" :warmer "#f0e698" :cooler "#d7d7b1" :faint "#b7b793")
+    :bg
+    (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48"))
+   :green
+   (:fg
+    (:main "#a9efa9" :warmer "#b1e698" :cooler "#98d7b1" :faint "#93b793")
+    :bg
+    (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48"))
+   :cyan
+   (:fg
+    (:main "#a9efef" :warmer "#b1e6d7" :cooler "#98d7f0" :faint "#93b7b7")
+    :bg
+    (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c"))
+   :blue
+   (:fg
+    (:main "#a9a9ef" :warmer "#b1a7d7" :cooler "#9898f0" :faint "#9393b7")
+    :bg
+    (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c"))
+   :magenta
+   (:fg
+    (:main "#efa9ef" :warmer "#f0a7d7" :cooler "#d798f0" :faint "#b793b7")
+    :bg
+    (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c"))
+   :red
+   (:fg
+    (:main "#efa9a9" :warmer "#f0a798" :cooler "#d798b1" :faint "#b79393")
+    :bg
+    (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848"))))
  :light
  (:bg
-  (:main "#deb3e6" :dim "#c8a2cf" :alt "#cfa3a2")
+  (:main "#e6ddb3" :dim "#e3d8a8" :alt "#cae3a8")
   :fg
-  (:main "#204b18" :dim "#375e30" :alt "#305c5f")))
+  (:main "#18214b" :dim "#1d2658" :alt "#371d59")
+  :colors
+  (:yellow
+   (:fg
+    (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48")
+    :bg
+    (:main "#efefaa" :warmer "#efe598" :cooler "#d6d7b0" :faint "#b7b793"))
+   :green
+   (:fg
+    (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48")
+    :bg
+    (:main "#aaefaa" :warmer "#b1e798" :cooler "#98d6b1" :faint "#93b793"))
+   :cyan
+   (:fg
+    (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c")
+    :bg
+    (:main "#aaefef" :warmer "#b2e7d8" :cooler "#98d5ef" :faint "#93b7b7"))
+   :blue
+   (:fg
+    (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c")
+    :bg
+    (:main "#aaaaef" :warmer "#b0a7d7" :cooler "#9898ef" :faint "#9393b7"))
+   :magenta
+   (:fg
+    (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c")
+    :bg
+    (:main "#efaaef" :warmer "#f0a8d6" :cooler "#d798ef" :faint "#b793b7"))
+   :red
+   (:fg
+    (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848")
+    :bg
+    (:main "#efaaaa" :warmer "#efa798" :cooler "#d698b1" :faint "#b79393")))))
+nil
 
-
-()
+(:dark
+ (:bg
+  (:main "#193b4c" :dim "#1c4357" :alt "#1b1f57" :colors
+         (:yellow
+          (:fg
+           (:main "#efefa9" :warmer "#f0e698" :cooler "#d7d7b1" :faint "#b7b793")
+           :bg
+           (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48"))
+          :green
+          (:fg
+           (:main "#a9efa9" :warmer "#b1e698" :cooler "#98d7b1" :faint "#93b793")
+           :bg
+           (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48"))
+          :cyan
+          (:fg
+           (:main "#a9efef" :warmer "#b1e6d7" :cooler "#98d7f0" :faint "#93b7b7")
+           :bg
+           (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c"))
+          :blue
+          (:fg
+           (:main "#a9a9ef" :warmer "#b1a7d7" :cooler "#9898f0" :faint "#9393b7")
+           :bg
+           (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c"))
+          :magenta
+          (:fg
+           (:main "#efa9ef" :warmer "#f0a7d7" :cooler "#d798f0" :faint "#b793b7")
+           :bg
+           (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c"))
+          :red
+          (:fg
+           (:main "#efa9a9" :warmer "#f0a798" :cooler "#d798b1" :faint "#b79393")
+           :bg
+           (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848"))))
+  :fg
+  (:main "#e6c3b3" :dim "#e2b9a7" :alt "#e2dca6"))
+ :light
+ (:bg
+  (:main "#b3d5e6" :dim "#a8cee3" :alt "#a8ace3" :colors
+         (:yellow
+          (:fg
+           (:main "#555510" :warmer "#675d0f" :cooler "#4e4e28" :faint "#6c6c48")
+           :bg
+           (:main "#efefaa" :warmer "#efe598" :cooler "#d6d7b0" :faint "#b7b793"))
+          :green
+          (:fg
+           (:main "#105510" :warmer "#316718" :cooler "#286741" :faint "#486c48")
+           :bg
+           (:main "#aaefaa" :warmer "#b1e798" :cooler "#98d6b1" :faint "#93b793"))
+          :cyan
+          (:fg
+           (:main "#105555" :warmer "#184d3e" :cooler "#0f4d67" :faint "#486c6c")
+           :bg
+           (:main "#aaefef" :warmer "#b2e7d8" :cooler "#98d5ef" :faint "#93b7b7"))
+          :blue
+          (:fg
+           (:main "#101055" :warmer "#312858" :cooler "#0f0f67" :faint "#48486c")
+           :bg
+           (:main "#aaaaef" :warmer "#b0a7d7" :cooler "#9898ef" :faint "#9393b7"))
+          :magenta
+          (:fg
+           (:main "#551055" :warmer "#570f3e" :cooler "#4e0f67" :faint "#6c486c")
+           :bg
+           (:main "#efaaef" :warmer "#f0a8d6" :cooler "#d798ef" :faint "#b793b7"))
+          :red
+          (:fg
+           (:main "#551010" :warmer "#671e0f" :cooler "#672841" :faint "#6c4848")
+           :bg
+           (:main "#efaaaa" :warmer "#efa798" :cooler "#d698b1" :faint "#b79393"))))
+  :fg
+  (:main "#4b2818" :dim "#582f1d" :alt "#59531d")))
+nil
