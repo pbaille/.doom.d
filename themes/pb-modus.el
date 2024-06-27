28407ab155d04344b4fe67a5a0f3b2b2ea4ffa1f
pbaille <pierrebaille@gmail.com>
2024-06-27 19:46:58 +0200

    pb-modus ++

--- themes/pb-modus.el
+++ themes/pb-modus.el
@@ -74,8 +74,8 @@
             (fg-dim ,(pc/from-hsl (list 0 0 .55)))
             (fg-faint ,(pc/from-hsl (list 0 0 .65)))
             (bg-intense ,(pc/from-hsl (list 0 0 1)))
-            (bg-main ,(pc/from-hsl (list 0 0 .95)))
-            (bg-dim ,(pc/from-hsl (list 0 0 .9)))
+            (bg-main ,(pc/from-hsl (list 0 0 .97)))
+            (bg-dim ,(pc/from-hsl (list 0 0 .94)))
             (bg-faint ,(pc/from-hsl (list 0 0 .8))))
           (pb-modus-build-colors)))
 
@@ -108,6 +108,17 @@
         (modeline-warning yellow)
         (modeline-info azure)
 
+        ;; Headings
+        (fg-heading-0 cyan-faint-lighter)
+        (fg-heading-1 rose-faint-lighter)
+        (fg-heading-2 violet-faint-lighter)
+        (fg-heading-3 cyan-faint-lighter)
+        (fg-heading-4 rose-faint-lighter)
+        (fg-heading-5 violet-faint-lighter)
+        (fg-heading-6 cyan-faint-lighter)
+        (fg-heading-7 rose-faint-lighter)
+        (fg-heading-8 violet-faint-lighter)
+
         ;; Code
         (builtin red-warmer-intense-lighter)
         (comment orange-lighter)
@@ -117,7 +128,7 @@
         (fnname magenta-faint-lighter)
         (keyword azure)
         (preprocessor red-cooler)
-        (string orange-warmer-intense)
+        (string orange-warmer-intense-darker)
         (type cyan-cooler)
         (variable spring-darker)
         (rx-construct green-cooler)
@@ -143,15 +154,18 @@
 
     (setq evil-normal-state-cursor `(box ,(pb-modus-get-color 'red-lighter))
           evil-insert-state-cursor `((bar . 3) ,(pb-modus-get-color 'cyan-intense-darker))
-          evil-visual-state-cursor `(box ,(pb-modus-get-color 'yellow-warmer-intense-lighter)))
+          evil-visual-state-cursor `(box ,(pb-modus-get-color 'yellow-warmer-intense-lighter))
+          evil-symex-state-cursor (list 'box (pb-modus-get-color 'cyan)))
 
     (set-face-attribute 'symex--current-node-face nil
-                        :background (pb-modus-get-color 'bg-dim))
+                        :background (pb-color_blend (pb-modus-get-color 'bg-main)
+                                                    (pb-modus-get-color 'cyan-faint-lighter)
+                                                    .9))
 
     (set-face-attribute 'default nil :foreground (pb-modus-get-color 'fg-main))
 
-    (setq evil-symex-state-cursor
-          (list 'box (pb-modus-get-color 'cyan)))
+    (set-face-attribute 'nerd-icons-ibuffer-file-face nil
+                        :foreground (pb-modus-get-color 'fg-dim))
 
     (set-face-attribute 'font-lock-delimiter-face nil
                         :foreground (pb-color (pb-modus-get-color 'bg-main)
@@ -164,8 +178,16 @@
     (set-face-attribute 'doom-modeline-bar nil
                         :background (pb-modus-get-color 'azure))
 
+    (set-face-attribute 'cider-result-overlay-face nil
+                        :box (list :line-width 2 :color (pb-modus-get-color 'bg-intense))
+                        :background (pb-modus-get-color 'bg-intense))
+
+    (set-face-attribute 'org-block-begin-line nil
+                        :foreground (pb-modus-get-color 'fg-faint))
+
     (setq ibuffer-filter-group-name-face
-          (list :foreground (pb-modus-get-color 'rose-warmer-faint-lighter)
+          (list :foreground (pb-modus-get-color 'orange-lighter)
+                :box (list :line-width 10 :color (pb-modus-get-color 'bg-main))
                 :weight 'bold
                 :height 1.1))
 
