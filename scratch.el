8c9fe604682c95cb5be7bb7085bd5d75439d2b0b
pbaille <pierrebaille@gmail.com>
2024-10-20 10:26:01 +0200

    cider repl window, blink window XP

--- scratch.el
+++ scratch.el
@@ -19,4 +19,52 @@ This only takes care of filling docstring correctly."
                  ? )))
 
 (setq-local adaptive-fill-function
+
             #'pb-clojure-adaptive-fill-function)
+
+(defun my/cider-choose-window-for-repl ()
+  "Select a window with ace-window to display the CIDER REPL."
+  (when-let ((repl-buffer (cider-current-repl)))
+    (let ((target-window (aw-select "Select a window for the REPL: ")))
+      (set-window-buffer target-window repl-buffer)
+      (select-window target-window))))
+
+(defun my/get-repl-target-windows ()
+  "Return a list of window candidates for displaying the REPL, excluding dired-sidebar-mode buffers."
+  (seq-filter
+   (lambda (win)
+     (not (eq 'dired-sidebar-mode (buffer-local-value 'major-mode (window-buffer win)))))
+   (window-list)))
+
+(defun my/cider-choose-window-for-repl ()
+  "Select a window with ace-window to display the CIDER REPL, excluding certain buffers."
+  (when-let ((repl-buffer (cider-current-repl)))
+    (let ((windows (my/get-repl-target-windows)))
+      (if (cdr windows)
+          (let ((target-window (aw-select "Select a window for the REPL: " )))
+            (set-window-buffer target-window repl-buffer)
+            (select-window target-window))))))
+
+(with-eval-after-load 'cider
+  (add-hook 'cider-repl-mode-hook #'my/cider-choose-window-for-repl))
+
+(remove-hook 'window-configuration-change-hook 'my-window-change-hook)
+
+(defun blink-window ()
+  "Blink the current window by temporarily changing the background color."
+  (interactive)
+  (let ((buffer (window-buffer (selected-window))))
+    (with-current-buffer buffer
+      (let ((remap-cookie (face-remap-add-relative 'default :background "LightSkyBlue1")))
+        (run-with-timer 0.2 nil
+                        (lambda (buf cookie)
+                          (with-current-buffer buf
+                            (face-remap-remove-relative cookie)))
+                        buffer remap-cookie)))))
+
+(defun my-window-change-hook (frame)
+  "Function to run when changing windows (FRAME is ignored)."
+  (blink-window))
+
+(add-hook 'window-selection-change-functions 'my-window-change-hook)
+(remove-hook 'window-selection-change-functions 'my-window-change-hook)
