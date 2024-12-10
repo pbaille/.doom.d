a84c4ac6f142c44d1355c3528d49daa9eec0affe
pbaille <pierrebaille@gmail.com>
2024-12-10 18:59:06 +0100

    dired omit all dotfiles

--- config/packages.el
+++ config/packages.el
@@ -57,7 +57,9 @@
 
     (setq delete-by-moving-to-trash t)
 
-    ;; (setq dired-omit-files ...)
+    (setq dired-omit-files "\\|^\\.[^.]")
+    '(setq dired-omit-files
+      (concat dired-omit-files "\\|^\\.[^.]"))
     (add-hook 'dired-mode-hook
               (lambda ()
                 (dired-omit-mode)
