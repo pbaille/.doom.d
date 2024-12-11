70f053de706b387b69e775026fddfa794349cd63
pbaille <pierrebaille@gmail.com>
2024-12-11 11:27:17 +0100

    org to split html export

--- pb/xp/org-to-directory.el
+++ pb/xp/org-to-directory.el
@@ -4,17 +4,21 @@
 ;; Keywords: org, markdown, export
 
 (require 'org)
+(require 'pb-org)
 (require 'ox-md)
+
+;;; Code:
 
 (setq org-export-with-broken-links t
       org-export-with-toc nil
       org-md-headline-style 'atx)
 
 (defun sanitize-filename (filename)
-  "Sanitize filename by replacing invalid characters with underscores."
+  "Sanitize FILENAME by replacing invalid characters with underscores."
   (replace-regexp-in-string "[^a-zA-Z0-9-_.]" "-" filename))
 
 (defun pb-org_file-split (file depth)
+  "Split an org FILE until DEPTH."
   (with-current-buffer (find-file-noselect file)
     (goto-char (point-min))
     (pb-org_first-node)
@@ -61,7 +65,8 @@
                      (pb-org_file-split file-name (1- depth))))
 
                  until
-                 (not (pb-org_forward)))))))
+                 (not (pb-org_forward)))
+        (save-buffer)))))
 
 '(progn
    (pb-org_file-split "~/.doom.d/pb/xp/split-xp.org"
@@ -69,40 +74,74 @@
    (pb-org_file-split "~/Code/WIP/noon/src/noon/doc/core.org"
                       1))
 
-(defun pb-org_convert-to-md-recursively (dir)
-  "Recursively export all org files in DIR to markdown."
-  (dolist (file (directory-files-recursively dir "\\.org$"))
-    (with-current-buffer (find-file-noselect file)
-      (org-md-export-to-markdown)
-      (delete-file file))))
+(progn :clj-doc
+       (defun pb-org_convert-to-md-recursively (dir)
+         "Recursively export all org files in DIR to markdown."
+         (dolist (file (directory-files-recursively dir "\\.org$"))
+           (with-current-buffer (find-file-noselect file)
+             (org-md-export-to-markdown)
+             (delete-file file))))
 
-(defun pb-org_mk-cljdoc (org-file output-dir depth)
-  (if (file-exists-p output-dir)
-      (delete-directory output-dir t t))
-  (make-directory output-dir t)
-  (with-current-buffer (find-file-noselect org-file)
-    (org-mode)
-    (goto-char (point-min))
-    (pb-org_first-node)
-    (let* ((title (org-get-heading t t t t))
-           (entry-point (expand-file-name (concat (sanitize-filename title) ".org") output-dir)))
-      (copy-file org-file entry-point)
-      (pb-org_file-split entry-point depth)))
-  '(let ((entry-point (expand-file-name (file-name-nondirectory org-file)
-                                        output-dir)))
-     (copy-file org-file entry-point)
-     (pb-org_file-split entry-point depth))
-  (pb-org_convert-to-md-recursively
-   output-dir))
+       (defun pb-org_mk-cljdoc (org-file output-dir depth)
+         "Create clj-doc ready folder (OUTPUT-DIR) based on ORG-FILE."
+         (if (file-exists-p output-dir)
+             (delete-directory output-dir t t))
+         (make-directory output-dir t)
+         (with-current-buffer (find-file-noselect org-file)
+           (org-mode)
+           (goto-char (point-min))
+           (pb-org_first-node)
+           (let* ((title (org-get-heading t t t t))
+                  (entry-point (expand-file-name (concat (sanitize-filename title) ".org") output-dir)))
+             (copy-file org-file entry-point)
+             (pb-org_file-split entry-point depth)))
+         '(let ((entry-point (expand-file-name (file-name-nondirectory org-file)
+                                               output-dir)))
+            (copy-file org-file entry-point)
+            (pb-org_file-split entry-point depth))
+         (pb-org_convert-to-md-recursively
+          output-dir))
 
-'(progn
-   (pb-org_mk-cljdoc
-    "~/Code/WIP/noon/src/noon/doc/guide.org"
-    "~/Code/WIP/noon/doc"
-    2)
-   (pb-org_convert-to-md-recursively
-    "~/Code/WIP/noon/doc/guide")
-   )
+       '(progn
+          (pb-org_mk-cljdoc
+           "~/Code/WIP/noon/src/noon/doc/guide.org"
+           "~/Code/WIP/noon/doc"
+           2)
+          (pb-org_convert-to-md-recursively
+           "~/Code/WIP/noon/doc/guide")
+          ))
+
+(progn :html-export
+
+       (defun pb-org_copy-and-split (org-file output-dir depth)
+         "Create clj-doc ready folder (OUTPUT-DIR) based on ORG-FILE."
+         (if (file-exists-p output-dir)
+             (delete-directory output-dir t t))
+         (make-directory output-dir t)
+         (with-current-buffer (find-file-noselect org-file)
+           (org-mode)
+           (goto-char (point-min))
+           (pb-org_first-node)
+           (let* ((title (org-get-heading t t t t))
+                  (entry-point (expand-file-name (concat (sanitize-filename title) ".org") output-dir)))
+             (copy-file org-file entry-point)
+             (pb-org_file-split entry-point depth))))
+
+       (defun pb-org_convert-to-html-recursively (dir)
+         "Recursively export all org files in DIR to HTML."
+         (dolist (file (directory-files-recursively dir "\\.org$"))
+           (with-current-buffer (find-file-noselect file)
+             (org-html-export-to-html)
+             (delete-file file))))
+
+       '(progn :do-it
+               (progn
+                 (pb-org_copy-and-split
+                  "~/Code/WIP/noon/src/noon/doc/guide.org"
+                  "~/Code/WIP/noon/public/guide/"
+                  2)
+                 (pb-org_convert-to-html-recursively
+                  "~/Code/WIP/noon/public/guide"))))
 
 (provide 'org-to-directory)
 
