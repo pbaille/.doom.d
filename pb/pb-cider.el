bd3c1bda80ff2df40ffc2cdaf9b30c7901b4ef1a
pbaille <pierrebaille@gmail.com>
2025-03-09 21:48:48 +0100

    pb-cider select repl buffer

--- pb/pb-cider.el
+++ pb/pb-cider.el
@@ -12,6 +12,7 @@
 ;;; Code:
 
 (require 'cider)
+(require 'consult)
 
 (defun pb-cider_eval! (code)
   "Eval some CODE using cider.
@@ -28,6 +29,32 @@ The `current-buffer' has to be a buffer recognized by cider for this to work,"
                (string-match-p "^\\*cider-repl" (buffer-name buffer))
                (not (process-live-p (get-buffer-process buffer))))
       (kill-buffer buffer))))
+
+(defun pb-cider_select-repl-buffer ()
+  "List and select a CIDER REPL buffer using consult."
+  (interactive)
+  (let* ((repl-buffers (seq-filter
+                        (lambda (buffer)
+                          (string-match-p "\\*cider-repl" (buffer-name buffer)))
+                        (buffer-list)))
+         (buffer-names (mapcar (lambda (buffer)
+                                 (let ((name (buffer-name buffer)))
+                                   (replace-regexp-in-string "\\*cider-repl \\([^:]*\\).*\\*" "\\1" name)))
+                               repl-buffers))
+         (buffer-alist (cl-mapcar #'cons buffer-names repl-buffers)))
+    (let ((selected (consult--read
+                     buffer-names
+                     :prompt "Select CIDER REPL: "
+                     :require-match t
+                     :sort nil)))
+      (buffer-name (cdr (assoc selected buffer-alist))))))
+
+(defun pb-cider_goto-repl ()
+  "Let the user choose a repl buffer and switch the buffer to it."
+  (interactive)
+  (let ((repl-buffer (pb-cider_select-repl-buffer)))
+    (when repl-buffer
+      (switch-to-buffer repl-buffer))))
 
 (provide 'pb-cider)
 ;;; pb-cider.el ends here.
