e442f8b6ab97d16d70dfa40586eeb37a944c8bd6
pbaille <pierrebaille@gmail.com>
2025-03-12 21:51:35 +0100

    add eval-pretty support for elisp

--- pb/pb-elisp.el
+++ pb/pb-elisp.el
@@ -11,6 +11,12 @@
 
 ;;; Code:
 
+(require 'symex)
+(require 'km)
+(require 'pb)
+
+(defvar pb-elisp_result-buffer-name "ELisp_eval")
+
 (defun pb-elisp_send-expression-to-ielm ()
   "Evaluate the expression at point and print the result in the IELM buffer."
   (interactive)
@@ -30,6 +36,36 @@
          ;; Extract the package name (you may need to adjust this based on your file naming convention)
          (package-prefix (concat base-name "_")))
     (insert package-prefix)))
+
+(defun pb-elisp_display-expression (expression)
+  "Display EXPRESSION in a dedicated buffer with proper formatting.
+This function creates or switches to a buffer named by
+`pb-elisp_result-buffer-name`, clears its content, inserts the
+pretty-printed EXPRESSION, sets it to `emacs-lisp-mode', and disables
+flycheck-mode."
+  (with-current-buffer (get-buffer-create pb-elisp_result-buffer-name)
+    (erase-buffer)
+    (insert (pp expression))
+    (emacs-lisp-mode)
+    (flycheck-mode -1)))
+
+(defun pb-elisp_eval-pretty ()
+  "Evaluate and pretty-print the current Lisp expression.
+Displays the result in a buffer named 'ELisp_eval'."
+  (interactive)
+  ;; symex is moving cursor ar the end of current symex before calling this
+  (backward-sexp)
+  ;; (pb-symex_select-current)
+  (pb-elisp_display-expression
+   (eval (read (pb-symex_current-as-string)))))
+
+;; extended the symex interface
+(setq symex-interfaces
+      `((emacs-lisp-mode . ,(km_put (alist-get 'emacs-lisp-mode symex-interfaces)
+                                    :eval-pretty
+                                    #'pb-elisp_eval-pretty))
+        ,@(seq-remove (pb_fn [(cons x _)] (equal x 'emacs-lisp-mode))
+                      symex-interfaces)))
 
 (provide 'pb-elisp)
 ;;; pb-elisp.el ends here.
