67e73136c7ba59119174d0a89a43f65041a60132
pbaille <pierrebaille@gmail.com>
2025-03-15 08:36:43 +0100

    add pb-struct_deftag

--- pb/pb-struct.el
+++ pb/pb-struct.el
@@ -68,9 +68,10 @@ Example:
   (pb-struct person name age)
 
   (pb-destructure '(person a b c) '(x z y))
+
   (cl-assert
    (equal
-    (pb_if [(person a b c) '(x z y)]
+    (pb_if [(person a b c) '(not-person x z y)]
            (list a b c)
            :ok)
     :ok))
@@ -95,14 +96,92 @@ Example:
    (equal
     (pb_let [(as emp
                  (employee x y (and (address? a)
-                                    (address ))))
+                                    (address street city))))
              (employee "Pierre"
                        43
                        (address
                         "821 Chemin de la chèvre d'or"
                         "Biot"))]
-        (list x y a))
-    '("Pierre" 43
+      (list x y street city a))
+    '("Pierre"
+      43
+      "821 Chemin de la chèvre d'or"
+      "Biot"
       (address "821 Chemin de la chèvre d'or" "Biot")))))
 
 (pb-struct_run-tests)
+
+(defmacro pb-struct_deftag (tag &optional content-pred)
+  "Define a cons structure tagged with TAG (which will be the car of the cons cell).
+This macro defines:
+- A constructor function named TAG that takes a value and returns a cons with TAG as car and the value as cdr
+- A predicate function named TAG? that checks if a value is a properly tagged cons with TAG
+- Support for using the tag in pattern matching via pb-destructure
+
+If CONTENT-PRED is provided, it will be used to validate the content before creating the tagged value.
+When the predicate returns nil, the constructor will also return nil.
+
+Example:
+  (pb-struct_deftag maybe)
+  (maybe 42)         ; => (maybe . 42)
+  (maybe? (maybe 42)) ; => t
+  (pb_let [(maybe x) (maybe 42)] x) ; => 42
+
+With validation:
+  (pb-struct_deftag numbox #'numberp)
+  (numbox 1)         ; => (numbox . 1)
+  (numbox \"hello\")  ; => nil (validation failed)"
+  (let* ((pred-sym (pb_join-symbol (list tag "?"))))
+    `(progn
+       (defun ,pred-sym (x)
+         ,(format "Return X if it is a %s tagged value." (pb_name tag))
+         (and (consp x)
+              (eq ',tag (car x))
+              ,(if content-pred
+                   `(funcall ,content-pred (cdr x))
+                 t)
+              x))
+
+       (defun ,tag (x)
+         ,(format "Create a new %s tagged value." (pb_name tag))
+         ,(if content-pred
+              `(when (funcall ,content-pred x)
+                 (cons ',tag x))
+            `(cons ',tag x)))
+
+       (pb-destructure_extend ',tag
+                              (lambda (args seed)
+                                (pb-destructure (car args)
+                                                (list 'cdr (list ',pred-sym seed))))))))
+
+(defun pb-struct_deftag-tests ()
+  ;; Test basic tag functionality with 'maybe' tag
+  (pb-struct_deftag maybe)
+
+  (cl-assert
+   (and
+    (pb_eq (maybe 42) '(maybe . 42) (cons 'maybe 42))
+    (maybe? (maybe 42))
+    (null (maybe? '(other . 42)))
+    (pb_eq (pb_let [(maybe x) (maybe 42)] x)
+           (pb_let [(maybe x) (cons 'maybe 42)] x)
+           (pb_if [(maybe x) (maybe 42)]
+                  x
+                  :not-maybe)
+           42)
+    (equal (pb_if [(maybe x) '(other . 42)]
+                  x
+                  :not-maybe)
+           :not-maybe)))
+
+  ;; Test tag with numeric validation
+  (pb-struct_deftag numbox #'numberp)
+
+  (cl-assert
+   (and
+    (pb_eq (numbox 1) (cons 'numbox 1))
+    (null (numbox 'io))
+    (equal 1 (pb_let [(numbox x) (numbox 1)] x))
+    (null (pb_let [(numbox x) (cons 'numbox 'io)] x)))))
+
+(pb-struct_deftag-tests)
