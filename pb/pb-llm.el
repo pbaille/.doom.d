c15bcd9fa64ac288bb30ebb2eebd1622695d46ee
pbaille <pierrebaille@gmail.com>
2025-03-15 20:57:47 +0100

    pb-llm WIP

--- pb/pb-llm.el
+++ pb/pb-llm.el
@@ -2,78 +2,105 @@
 
 (require 'request)
 (require 'json)
+(require 'vec)
 
 (progn
   (defvar pb-llm_chats
     ())
 
-  (defun pb-llm_create-chat (id)
-    (setq pb-llm_chats
-          (km_put pb-llm_chats
-                  id
-                  (km :options
-                      (km :url "https://api.anthropic.com/v1/messages"
-                          :api-key pb/claude-api-key
-                          :model "claude-3-7-sonnet-20250219"
-                          :max-tokens 10000)
+  (defvar pb-llm_last-responses
+    ())
 
-                      :messages
-                      (list (km :role "user"
-                                :content "You are firendly LLM"))))))
+  (defun pb-llm_create-chat (id system-prompt)
+    (pb_setq pb-llm_chats
+             (km_put _
+                     id
+                     (km :options
+                         (km :url "https://api.anthropic.com/v1/messages"
+                             :api-key pb/claude-api-key
+                             :model "claude-3-7-sonnet-20250219"
+                             :max-tokens 10000)
+
+                         :system
+                         (vector
+                          (km :type "text"
+                              :text system-prompt
+                              :cache_control (km :type "ephemeral")))
+
+                         :messages ()))))
+
+  (pb-llm_create-chat :simple
+                      "You are a useful assistant, you leave in emacs.")
+
+  (defun pb-llm_cache-context (id prompt)
+    "Add a new element to the :messages entry of the ID chat,")
 
   (defun pb-llm_chat (chat-id prompt)
-    (pb_if [(km_keys options messages) (km_get pb-llm_chats chat-id)]
-           (pb_let [(km_keys url api-key max-tokens model) options
+    (pb_if [chat (km_get pb-llm_chats chat-id)]
+           (pb_let [(km_keys options system messages) chat
+                    (km_keys url api-key max-tokens model) options
                     data (json-encode
                           (km :model model
                               :max_tokens max-tokens
-                              :messages (vconcat messages
-                                                 (list (km :role "user"
-                                                           :content prompt)))))]
-               (request
-                 url
-                 :type "POST"
-                 :headers `(("Content-Type" . "application/json")
-                            ("x-api-key" . ,api-key)
-                            ("anthropic-version" . "2023-06-01"))
-                 :data (json-encode
-                        (km :model model
-                            :max_tokens max-tokens
-                            :messages (vconcat messages
-                                               (list (km :role "user"
-                                                         :content prompt)))))
-                 :parser #'json-read
-                 :success (lambda (&rest args)
-                            (pb_let [response
-                                     (pb-> (km_get args :data)
-                                           (km_from-alist)
-                                           (km_select-paths :role :content)
-                                           (km_upd :content
-                                                   (pb_fn [content]
-                                                          (vconcat (mapcar #'km_from-alist content)))))]
-                                (setq pb-llm_chats
-                                      (km_upd pb-llm_chats
-                                              (list chat-id :messages)
-                                              (pb_fn [messages]
-                                                     (vconcat messages
-                                                              (list response)))))))
-                 :error (cl-function
-                         (lambda (&key data &allow-other-keys)
-                           (message "Error calling Anthropic API: %S" data))))
+                              :messages (vec_conj messages
+                                                  (km :role "user"
+                                                      :content prompt))))]
+             (request
+               url
+               :type "POST"
+               :headers `(("Content-Type" . "application/json")
+                          ("x-api-key" . ,api-key)
+                          ("anthropic-version" . "2023-06-01"))
+               :data (json-encode
+                      (km :model model
+                          :max_tokens max-tokens
+                          :system system
+                          :messages (vec_conj messages
+                                              (km :role "user"
+                                                  :content prompt))))
+               :parser #'json-read
+               :success (lambda (&rest args)
+                          (pb_setq pb-llm_last-responses
+                                   (cons (km_get args :data) _))
+                          (pb_let [response
+                                   (pb-> (km_get args :data)
+                                         (km_from-alist)
+                                         (km_select-paths :role :content)
+                                         (km_upd :content
+                                                 (pb_fn [content]
+                                                        (vec (mapcar #'km_from-alist content)))))]
+                            (pb_setq pb-llm_chats
+                                     (km_upd _
+                                             (list chat-id :messages)
+                                             (pb_fn [messages]
+                                                    (vec_conj messages
+                                                              response))))))
+               :error (cl-function
+                       (lambda (&key data &allow-other-keys)
+                         (message "pb-llm_chat request error: %S"
+                                  data))))
              :ok)
+
            (progn
-             (pb-llm_create-chat chat-id)
+             (pb-llm_create-chat chat-id "You are a useful assistant, you leave in emacs.")
              (pb-llm_chat chat-id prompt)))))
 
 (pb_comment
- (pb-llm_create-chat :first)
- (pb-llm_chat :first
+ (km_get pb-llm_chats :simple)
+ (km_get pb-llm_chats :pb-llm)
+ pb-llm_last-responses
+
+ (pb-llm_create-chat :pb-llm
+                     (pb-gptel_mk-request-prompt [:code :lisp :context]))
+
+ (pb-llm_chat :pb-llm
+              "Explain the pb-llm package purpose")
+
+ (pb-llm_chat :simple
               "I'd like to speak about common lisp")
 
- (pb-llm_chat :first
+ (pb-llm_chat :simple
               "maybe how it compares to clojure")
-
- (km_get pb-llm_chats :first)
 
  (json-encode (km_get pb-llm_chats :first)))
 
