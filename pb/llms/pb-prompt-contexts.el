e9d578a890064f36756a0db8de1de36d22329f03
pbaille <pierrebaille@gmail.com>
2025-04-21 14:06:54 +0200

    Add saved contexts file for pb-prompt

---
+++ pb/llms/pb-prompt-contexts.el
@@ -0,0 +1,19 @@
+;; pb-prompt saved contexts - automatically generated
+
+(setq pb-prompt/saved-contexts (make-hash-table :test 'equal))
+
+(puthash "pb el base" '(
+  (:type "file" :path "~/.doom.d/pb/pb.el" :id "2100d854")
+  (:type "file" :path "~/.doom.d/pb/pb-destructure.el" :id "f1ff76ed")
+  (:type "file" :path "~/.doom.d/pb/sq.el" :id "ec1142b4")
+  (:type "file" :path "~/.doom.d/pb/km.el" :id "b6645970"))
+ pb-prompt/saved-contexts)
+
+(puthash "pb el extended" '(
+  (:type "file" :path "~/.doom.d/pb/pb-lisp.el" :id "4df02cbe")
+  (:type "file" :path "~/.doom.d/pb/pb.el" :id "2100d854")
+  (:type "file" :path "~/.doom.d/pb/pb-destructure.el" :id "f1ff76ed")
+  (:type "file" :path "~/.doom.d/pb/sq.el" :id "ec1142b4")
+  (:type "file" :path "~/.doom.d/pb/km.el" :id "b6645970"))
+ pb-prompt/saved-contexts)
+
