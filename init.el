a9ef2320621244df95ebe8e0aac9d1cf8084f90e
pbaille <pierrebaille@gmail.com>
2025-10-25 20:55:19 +0200

    bring back lsp

--- init.el
+++ init.el
@@ -100,7 +100,7 @@
        (eval +overlay)       ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup                   ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit                    ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -120,7 +120,8 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        cc                               ; C > C++ == 1
-       clojure                          ; java with a lisp
+                                 ; java with a lisp
+       (clojure +lsp)
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
